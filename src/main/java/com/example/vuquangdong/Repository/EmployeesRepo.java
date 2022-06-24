7d57a2f40de76d353699fbec34e4937f7166522d
conquy2002 <72489231+conquy2002@users.noreply.github.com>
2022-06-24 15:32:59 +0700

    new

--- src/main/java/com/example/vuquangdong/Repository/EmployeesRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.vuquangdong.Repository;
-
-import com.example.vuquangdong.Entity.EmployeesEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeesRepo extends JpaRepository<EmployeesEntity,Integer> {
-}
